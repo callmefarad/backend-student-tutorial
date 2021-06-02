211cb3fb24cb8b590c9c327bda2218bd01e90a17
callmefarad <65671589+callmefarad@users.noreply.github.com>
2021-06-02 15:45:14 +0100

    Express endpoint

--- express/app.js
+++ express/app.js
@@ -31,16 +31,31 @@ app.get("/api/student/:id", (req, res) => {
 
 // POST REQUEST
 app.post("/api/student", (req, res) => {
-  if (
-    !req.body.name ||
-    !req.body.course ||
-    req.body.name.length < 3 ||
-    req.body.course.length < 3
-  ) {
-    res.status(400).send("Field can't be empty or less than 3 characters'");
+  // if (
+  //   !req.body.name ||
+  //   !req.body.course ||
+  //   req.body.name.length < 3 ||
+  //   req.body.course.length < 3
+  // ) {
+  //   res.status(400).send("Field can't be empty or less than 3 characters'");
+  //   return;
+  // }
+
+  // using joi to validate the user entry
+  // create a schema
+  const schema = {
+    name: Joi.string().min(3).max(20).required(),
+    course: Joi.string().min(3).max(20).required(),
+  };
+
+  // validate the request body based on the schema
+  const result = Joi.validate(req.body, schema);
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
     return;
   }
 
+  // create a new student object
   const stud = {
     id: codelab.length + 1,
     name: req.body.name,
@@ -48,7 +63,62 @@ app.post("/api/student", (req, res) => {
   };
   codelab.push(stud);
   res.send(stud);
+  console.log(stud);
 });
+
+// updating a student - PUT REQUEST
+app.put("/api/student/:id", (req, res) => {
+  // find the course
+  const stud = codelab.find((stu) => stu.id === parseInt(req.params.id));
+  // check for 404 errors
+  if (!stud) {
+    res.status(404).send(`No such student with id: ${req.params.id}`);
+  }
+
+  // const schema = {
+  //   name: Joi.string().min(3).max(20).required(),
+  //   course: Joi.string().min(3).max(20).required(),
+  // };
+  // // validate the course
+  // const result = Joi.validate(req.body, schema);
+  // // send the updated course
+  // if (result.error) {
+  //   res.status(400).send(result.error.details[0].message);
+  //   return;
+  // }
+
+  const { error } = validateStudent(req.body);
+  if (error) {
+    res.status(400).send(error.details[0].message);
+    return;
+  }
+  stud.name = req.body.name;
+  stud.course = req.body.course;
+  res.send(stud);
+});
+
+// delete student route - DELETE
+app.delete("/api/student/:id", (req, res) => {
+  const stud = codelab.find((stu) => stu.id === parseInt(req.params.id));
+  if (!stud) {
+    res.status(404).send(`No such student with id: ${req.params.id}`);
+  }
+
+  // get the index position of the student
+  const studentPosition = codelab.indexOf(stud);
+  // delete index specified by studentPosition and display only the index value
+  codelab.splice(studentPosition, 1);
+  res.status(200).send(stud);
+});
+
+// function for validating student.
+const validateStudent = (student) => {
+  const schema = {
+    name: Joi.string().min(3).max(20).required(),
+    course: Joi.string().min(3).max(20).required(),
+  };
+  return Joi.validate(student, schema);
+};
 
 app.listen(port, () => {
   console.log(`app is listening to port: ${port}`);
--- express2/app.js
+++ express2/app.js
@@ -1,31 +1,50 @@
 const express = require("express");
 const port = 4000;
 
-const students = [
-  { id: 1, name: "john", course: "web" },
-  { id: 2, name: "judith", course: "mobile" },
-  { id: 3, name: "ben", course: "mobile" },
-  { id: 4, name: "kate", course: "mobile" },
-  { id: 5, name: "emma", course: "web" },
-];
-
 const app = express();
 app.use(express.json());
+
+const gamers = [
+  { id: 1, name: "chidi", game: "acade" },
+  { id: 2, name: "romanus", game: "adventure" },
+  { id: 3, name: "lebile", game: "soccer" },
+  { id: 4, name: "judith", game: "action" },
+  { id: 5, name: "cynthia", game: "racing" },
+];
+
+// default view
 app.get("/", (req, res) => {
-  res.send("using express");
+  res.send("Hello am coding nodejs");
 });
 
-app.get("/api/students", (req, res) => {
-  res.send(students);
+//show all gamers
+app.get("/api/gamers", (req, res) => {
+  res.send(gamers);
 });
 
-app.get("/api/students/:id", (req, res) => {
-  const student = students.find((x) => x.id === parseInt(req.params.id));
-  if (!student) {
-    res.send(`Invalid id: ${req.params.id}`);
+// show a single gamer
+app.get("/api/gamers/:id", (req, res) => {
+  const gamerId = gamers.find((user) => user.id === parseInt(req.params.id));
+  if (!gamerId) {
+    res.status(404).send(`NO user with this id: ${req.params.id}`);
   }
-  res.send(student);
+  res.status(200).send(gamerId);
 });
+
+// create new gamer
+app.post("/api/gamers", (req, res) => {
+  if (!req.body.name || !req.body.game) {
+    return res.status(400).send("cant be blank");
+  }
+  const newGamer = {
+    id: gamers.length + 1,
+    name: req.body.name,
+    game: req.body.game,
+  };
+  gamers.push(newGamer);
+  res.status(200).send(newGamer);
+});
+
 app.listen(port, () => {
   console.log(`server is listening on port: ${port}`);
 });
